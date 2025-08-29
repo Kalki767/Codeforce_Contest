70589ce82000bb422e5e2bcd1df64e6f8b509e60
Kalki767 <kalkidankidane3@gmail.com>
2025-08-29 21:54:37 +0300

    solution for number of minimums on a segment problem

--- Segment Tree/Part I/C_Number_of_Minimums_on_a_Segment.py
+++ Segment Tree/Part I/C_Number_of_Minimums_on_a_Segment.py
@@ -1,58 +1,61 @@
-class SegmentTree():
-    def __init__(self,n,arr, default, func = max, ):
-        self.tree = [[float('inf'),0] for _ in range(2*n)]
+class SegmentTree :
+    def __init__(self,n, default,arr):
         self.n = n
-        self.function = func
+        self.arr = arr
+        self.tree = [[default,0] for _ in range(2*self.n)]
 
-        #build the tree from the ground
         for index in range(n):
-            self.tree[index + n] = [arr[index],1]
-
-        for index in range(n-1,-1,-1):
+            self.tree[index+n] = [self.arr[index],1]
+        
+        for index in range(n-1,0,-1):
             self.tree[index] = self.combine(self.tree[index << 1], self.tree[index << 1 | 1])
 
-    def combine(self,left,right):
+        # print(self.tree) 
+    
+    def combine(self, left, right):
         if left[0] < right[0]:
             return left
-        if right[0] < left[0]:
+        elif left[0] > right[0]:
             return right
-        return [left[0], left[1] + right[1]] 
-    
+        return [left[0], left[1] + right[1]]
+        
     def update(self,index,value):
+        self.arr[index] = value
+
         index += self.n
-        self.tree[index] = [value, 1]
+        self.tree[index] = [value,1]
 
         while index > 1:
-            self.tree[index >> 1] = self.combine(self.tree[index],self.tree[index ^ 1])
+            self.tree[index >> 1] = self.combine(self.tree[index], self.tree[index ^ 1])
             index >>= 1
-    
-    def query(self,l,r,default):
-        left, right = self.n + l, self.n + r
+        
+    def query(self,left,right):
+        answer = [float('inf'), 0]
 
-        result = [float('inf'),0]
+        left += self.n
+        right += self.n
+
         while left < right:
             if left & 1:
-                result = self.combine(result,self.tree[left])
+                answer = self.combine(answer, self.tree[left])
                 left += 1
-            
             if right & 1:
+                answer = self.combine(answer, self.tree[right - 1])
                 right -= 1
-                result = self.combine(result, self.tree[right])
             
             left >>= 1
             right >>= 1
         
-        return result
-
-
+        return answer
 
 n, m = map(int,input().split())
 arr = list(map(int,input().split()))
-segment_tree = SegmentTree(n,arr,float('inf'),min)
+tree = SegmentTree(n,float('inf'),arr)
+
 for _ in range(m):
-    operation, u, v = map(int,input().split())
-    if operation == 2:
-        print(*(segment_tree.query(u,v,float('inf'))))
+    op, first, second = map(int,input().split())
+    if op == 1:
+        tree.update(first,second)
     else:
-        arr[u] = v
-        segment_tree.update(u,v)
+        ans = tree.query(first,second)
+        print(*ans)
